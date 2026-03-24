31f6ebbaa0058f0cd342e549d53f4709937bf343
laitiii <iiro.e.laitinen@helsinki.fi>
2026-03-24 16:42:10 +0200

    Update tests for maksukortti

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,22 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    def test_kortti_latautuu_oikein(self):
+        self.maksukortti.lataa_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo, 1500)
+
+    def test_kortilta_veloitetaan_oikein(self):
+        self.maksukortti.ota_rahaa(200)
+        self.assertEqual(self.maksukortti.saldo, 800)
+
+    def test_kortilta_veloitetaan_liikaa(self):
+        self.maksukortti.ota_rahaa(1200)
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    def test_kortti_ilmoittaa_tapahtuman_onnistumisesta(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(500))
+        self.assertFalse(self.maksukortti.ota_rahaa(600))
