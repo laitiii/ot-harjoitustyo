5c982ddf62e9ca93064d5a0fa66207675dec423f
laitiii <iiro.e.laitinen@helsinki.fi>
2026-03-31 20:49:15 +0300

    Add invoke tasks

---
+++ tasks.py
@@ -0,0 +1,14 @@
+from invoke import task
+
+@task
+def start(c):
+    c.run("python src/index.py", pty=True)
+
+@task
+def test(c):
+    c.run("pytest src", pty=True)
+
+@task
+def coverage_report(c):
+    c.run("coverage run --branch -m pytest src", pty=True)
+    c.run("coverage html", pty=True)
