7ae740798454ed263889e9aa48ed11d5c7382ba8
laitiii <iiro.e.laitinen@helsinki.fi>
2026-04-21 23:08:13 +0300

    Add more tests for game logic

--- src/tests/test_pytd.py
+++ src/tests/test_pytd.py
@@ -1,4 +1,5 @@
 import unittest
+import pygame
 from index import PyTD
 
 
@@ -12,6 +13,65 @@ class TestPyTD(unittest.TestCase):
         game = PyTD()
         self.assertEqual(len(game.level_map), 10)
         self.assertEqual(len(game.level_map[0]), 10)
+
+    def test_place_tower_reduces_money_and_adds_tower(self):
+        game = PyTD()
+        initial_money = game.money
+        game.state = "game"
+
+        game.place_tower((0, 0))
+
+        self.assertEqual(len(game.towers), 1)
+        self.assertEqual(game.money, initial_money - 50)
+        self.assertEqual((game.towers[0].x, game.towers[0].y), (0, 0))
+
+    def test_invalid_tower_placement(self):
+        game = PyTD()
+        initial_money = game.money
+        game.state = "game"
+
+        game.place_tower((64, 64))
+
+        self.assertEqual(len(game.towers), 0)
+        self.assertEqual(game.money, initial_money)
+
+    def test_new_game_initializes_path_and_level_map(self):
+        game = PyTD()
+        game.level_map[0][0] = 1
+        game.path = []
+
+        game.new_game()
+
+        self.assertEqual(game.path[0], (0, 1))
+        self.assertEqual(len(game.level_map), 10)
+        self.assertEqual(game.level_map[0][0], 0)
+
+    def test_state_change_on_game_over(self):
+        game = PyTD()
+        game.state = "game"
+        game.lives = 0
+
+        game.update()
+
+        self.assertEqual(game.state, "menu")
+
+    def test_event_handler_starts_new_game(self):
+        game = PyTD()
+        game.state = "menu"
+        game.lives = 5
+        game.money = 10
+        game.towers = [1]
+        game.enemies = [1]
+
+        pygame.event.clear()
+        pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_SPACE))
+        game.event_handler()
+
+        self.assertEqual(game.state, "game")
+        self.assertEqual(game.lives, 10)
+        self.assertEqual(game.money, 100)
+        self.assertEqual(len(game.towers), 0)
+        self.assertEqual(len(game.enemies), 0)
 
 
 if __name__ == "__main__":
---
+++ src/tests/test_tower.py
@@ -0,0 +1,19 @@
+import unittest
+from tower import Tower
+from enemy import Enemy
+
+
+class TestTower(unittest.TestCase):
+
+    def test_tower_hits_enemy_in_range_and_sets_cooldown(self):
+        tower = Tower(0, 0)
+        enemy = Enemy(1, 1)
+
+        tower.update([enemy])
+
+        self.assertEqual(enemy.health, 2)
+        self.assertEqual(tower.cooldown, tower.fire_rate)
+
+
+if __name__ == "__main__":
+    unittest.main()
