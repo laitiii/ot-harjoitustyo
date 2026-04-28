64b7c9653d638062280a872f882cca41a0d9ff4d
faintworks <iiro.e.laitinen@helsinki.fi>
2026-04-28 17:19:17 +0300

    Update pty to be conditional depending on system platform

--- tasks.py
+++ tasks.py
@@ -1,18 +1,20 @@
 from invoke import task
+import sys
+
+pty = sys.platform != "win32"
 
 @task
 def start(c):
-    c.run("python src/index.py", pty=True)
-
+    c.run("python src/index.py", pty=pty)
 @task
 def test(c):
-    c.run("pytest src", pty=True)
+    c.run("pytest src", pty=pty)
 
 @task
 def coverage_report(c):
-    c.run("coverage run --branch -m pytest src", pty=True)
-    c.run("coverage html", pty=True)
+    c.run("coverage run --branch -m pytest src", pty=pty)
+    c.run("coverage html", pty=pty)
 
 @task
 def pylint(c):
-    c.run("pylint src", pty=True)
+    c.run("pylint src", pty=pty)
