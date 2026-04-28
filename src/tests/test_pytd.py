6c1fb629d5763e268af75c7694b0f34a152fd995
laitiii <iiro.e.laitinen@helsinki.fi>
2026-04-28 22:51:42 +0300

    Add test for game.update()

--- src/tests/test_pytd.py
+++ src/tests/test_pytd.py
@@ -88,6 +88,18 @@ class TestPyTD(unittest.TestCase):
         self.assertEqual(len(game.enemies), 1)
         self.assertEqual(game.wave_enemies_pending, 1)
 
+    def test_update_spawns_pending_enemy_after_interval(self):
+        game = PyTD()
+        game.state = "game"
+        game.enemies = [Enemy(0, 1)]
+        game.wave_enemies_pending = 1
+        game.next_spawn_time = 0
+
+        game.update()
+
+        self.assertEqual(len(game.enemies), 2)
+        self.assertEqual(game.wave_enemies_pending, 0)
+
     def test_spawn_wave_queues_remaining_enemies(self):
         game = PyTD()
         game.wave = 1
