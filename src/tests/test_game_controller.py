966cf3ea028e8630180c453529c4afa9a98ad833
laitiii <iiro.e.laitinen@helsinki.fi>
2026-05-10 23:33:02 +0300

    Split tests to match game logic

--- src/tests/test_game_controller.py
+++ src/tests/test_game_controller.py
@@ -4,7 +4,7 @@ from entities.enemy import Enemy
 from index import PyTD
 
 
-class TestPyTD(unittest.TestCase):
+class TestGameController(unittest.TestCase):
 
     def test_can_create_game(self):
         game = PyTD()
@@ -73,57 +73,6 @@ class TestPyTD(unittest.TestCase):
         self.assertEqual(game.lives, 10)
         self.assertEqual(game.money, 100)
         self.assertEqual(len(game.towers), 0)
-        self.assertEqual(len(game.enemies), 0)
-
-    def test_event_handler_starts_wave_from_build(self):
-        game = PyTD()
-        game.state = "build"
-        game.wave = 1
-
-        pygame.event.clear()
-        pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_SPACE))
-        game.event_handler()
-
-        self.assertEqual(game.state, "game")
-        self.assertEqual(len(game.enemies), 1)
-        self.assertEqual(game.wave_manager.wave_enemies_pending, 2)
-
-    def test_update_spawns_pending_enemy_after_interval(self):
-        game = PyTD()
-        game.state = "game"
-        game.enemies = [Enemy(0, 1)]
-        game.wave_manager.wave_enemies_pending = 1
-        game.wave_manager.next_spawn_time = 0
-
-        game.update()
-
-        self.assertEqual(len(game.enemies), 2)
-        self.assertEqual(game.wave_manager.wave_enemies_pending, 0)
-
-    def test_spawn_wave_queues_remaining_enemies(self):
-        game = PyTD()
-        game.wave = 1
-        game.start_wave()
-        self.assertEqual(len(game.enemies), 1)
-        self.assertEqual(game.wave_manager.wave_enemies_pending, 2)
-
-        game.wave = 2
-        game.start_wave()
-        self.assertEqual(len(game.enemies), 1)
-        self.assertEqual(game.wave_manager.wave_enemies_pending, 4)
-
-    def test_wave_completion_returns_to_build(self):
-        game = PyTD()
-        game.state = "game"
-        game.wave = 1
-        enemy = Enemy(0, 1)
-        enemy.health = 0
-        game.enemies = [enemy]
-
-        game.update()
-
-        self.assertEqual(game.state, "build")
-        self.assertEqual(game.wave, 2)
         self.assertEqual(len(game.enemies), 0)
 
 
